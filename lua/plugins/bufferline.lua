896abcdb1a6e968e043f687c4e02ca4260927acf
kaiyulee <sky.craper@foxmail.com>
2024-04-20 14:02:30 +0800

    feat: adjust bufferline, add terminal

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -7,7 +7,13 @@ return {
 		vim.api.nvim_set_keymap("n", "<Tab>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
 		vim.api.nvim_set_keymap("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
 		require("bufferline").setup({
-			options = {},
+			options = {
+				indicator = {
+					icon = "",
+					style = "underline",
+				},
+				buffer_close_icon = "",
+			},
 		})
 	end,
 }
